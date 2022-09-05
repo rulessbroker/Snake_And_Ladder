6dfeb028f6e2aaead8b331cff64e014dd0758e99
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-05 17:04:09 +0530

    Check Option NoPlay, Ladder or Snake

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,20 +2,43 @@ package Snake_and_Ladder;
 
 public class SnakeAndLadder {
 	
-	static int startPosition = 0;
+	static final int startPosition = 0;
+	static final int snakeOption = 1;
+	static final int ladderOption = 2;
 	
 	public static void main(String[] args) {
 		
+		int dieNumber, option;
 		int position = startPosition;
 		
-		System.out.println("Current Position = " +position);
-		
-		int dieNumber=rollDie();
-
+		 dieNumber = rollDie();
 	        System.out.println("Die Number = "+dieNumber);
+	        
+	        option=checkOption();
+
+	        switch (option){
+	            case ladderOption:
+	                System.out.println("ladder option");
+	                position += dieNumber;
+	                break;
+	            case snakeOption:
+	                System.out.println("snake option");
+	                position -= dieNumber;
+	                break;
+	            default:
+	                System.out.println("No Play option");
+	                break;
+	        }
+	        
+	        System.out.println("current position : "+position);
+	        
 	    }
 	    static int rollDie(){
-	        int random= (int) (Math.random()*6) + 1;
+	        int random = (int) (Math.random()*6) + 1;
 	        return random;
 	}
+	    static int checkOption(){
+	        int random = (int) (Math.random()*3);
+	        return random;
+	    }
 }
