9f33b74824e8c1f7309a45160aa4c81524ce94f1
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-05 16:42:12 +0530

    Start Position Is Zero

---
+++ SnakeAndLadder.java
@@ -0,0 +1,13 @@
+package Snake_and_Ladder;
+
+public class SnakeAndLadder {
+	
+	static int startPosition = 0;
+	
+	public static void main(String[] args) {
+		
+		int position = startPosition;
+		
+		System.out.println("Current Position = " +position);
+	}
+}
