5fd6c39a986cf014696236cf5b23e152998ca2fa
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-05 17:32:11 +0530

    Play With Two Player

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,53 +5,90 @@ public class SnakeAndLadder {
 	static final int startPosition = 0;
 	static final int snakeOption = 1;
 	static final int ladderOption = 2;
+	static final int vikram = 1;
+    static final int vedha = 2;
 	
 	public static void main(String[] args) {
 		
 		int dieNumber, option;
-		int position = startPosition;
 		int dieCount = 0;
-		
-		 while (position<100){
-			 dieNumber = rollDie();
-			 dieCount++;
-	         System.out.println("Die Number = "+dieNumber);
-	        
-	       
-	        	option=checkOption();
+		int vikramPosition=startPosition;
+        int vedhaPosition=startPosition;
+        int currentPlayer=vikram;
 
-		        switch (option){
-		            case ladderOption:
-		                System.out.println("ladder option");
-		                position += dieNumber;
-		                break;
-		            case snakeOption:
-		                System.out.println("snake option");
-		                position -= dieNumber;
-		                break;
-		            default:
-		                System.out.println("No Play option");
-		                break;
-		        }
-	        if (position < 0){
-                position = 0;
-            } else if (position>100) {
-                position -= dieNumber;
+
+        while (vikramPosition<100 && vedhaPosition<100){
+            dieNumber=rollDie();
+            dieCount++;
+            System.out.println("#"+dieCount);
+            System.out.println("Die Number = "+dieNumber);
+
+            option=checkOption();
+
+            switch (option){
+                case ladderOption:
+                    System.out.println("ladder option");
+                    if (currentPlayer == vikram) {
+                        vikramPosition+=dieNumber;
+                    }else {
+                        vedhaPosition+=dieNumber;
+                    }
+                    break;
+                case snakeOption:
+                    System.out.println("snake option");
+                    if (currentPlayer == vikram) {
+                        vikramPosition-=dieNumber;
+                    }else {
+                        vedhaPosition-=dieNumber;
+                    }
+                    currentPlayer=switchPlayer(currentPlayer);
+                    break;
+                default:
+                    System.out.println("No Play option");
+                    currentPlayer=switchPlayer(currentPlayer);
+                    break;
             }
-	        
-            System.out.println("current position : "+position);
-	        
-	        }
-		 	
-		 System.out.println("Number of times die played = "+dieCount);
-	        
-	    }
-	    static int rollDie(){
-	        int random = (int) (Math.random()*6) + 1;
-	        return random;
-	}
-	    static int checkOption(){
-	        int random = (int) (Math.random()*3);
-	        return random;
-	    }
+
+            //ensure start position and exact winning position
+            if (vikramPosition < 0){
+                vikramPosition=0;
+            } else if (vedhaPosition < 0){
+                vedhaPosition=0;
+            }else if (vikramPosition>100) {
+                vikramPosition-=dieNumber;
+            } else if (vedhaPosition>100) {
+                vedhaPosition-=dieNumber;
+            }
+
+            System.out.println("current Vikram's position : "+vikramPosition);
+            System.out.println("current Vedha's position : "+vedhaPosition);
+
+
+        }
+        System.out.println("*******************************");
+        showWinner(vikramPosition,vedhaPosition);
+        System.out.println("Number of times die played = "+dieCount);
+    }
+
+    static int rollDie(){
+        return (int) (Math.random()*6) +1;
+    }
+    static int checkOption(){
+        return (int) (Math.random()*3);
+    }
+    static int switchPlayer(int currentPlayer){
+        if (currentPlayer == vikram) {
+            return vedha;
+        }else {
+            return vikram;
+        }
+    }
+
+    static void showWinner(int vikram, int vedha){
+        if (vikram==100){
+            System.out.println("Vikram is Winner");
+        } else if (vedha==100) {
+            System.out.println("Vedha is Winner");
+        }
+    }
 }
