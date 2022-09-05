d029b57bdcba0e8270a9e9d84a9a60214b5000bf
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-05 17:41:47 +0530

    Edit code For maintain Hygine

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -2,69 +2,72 @@ package Snake_and_Ladder;
 
 public class SnakeAndLadder {
 	
-	static final int startPosition = 0;
-	static final int snakeOption = 1;
-	static final int ladderOption = 2;
-	static final int vikram = 1;
-    static final int vedha = 2;
-	
+		static final int startPosition = 0;
+		static final int snakeOption = 1;
+		static final int ladderOption = 2;
+		static final int vikram = 1;
+	    static final int vedha = 2;
+		
 	public static void main(String[] args) {
 		
-		int dieNumber, option;
-		int dieCount = 0;
-		int vikramPosition=startPosition;
-        int vedhaPosition=startPosition;
-        int currentPlayer=vikram;
+			int dieNumber, option;
+			int dieCount = 0;
+			int vikramPosition = startPosition;
+	        int vedhaPosition = startPosition;
+	        int currentPlayer = vikram;
 
 
-        while (vikramPosition<100 && vedhaPosition<100){
-            dieNumber=rollDie();
+        while (vikramPosition < 100 && vedhaPosition < 100){
+            dieNumber = rollDie();
             dieCount++;
             System.out.println("#"+dieCount);
             System.out.println("Die Number = "+dieNumber);
 
-            option=checkOption();
+            option = checkOption();
 
             switch (option){
                 case ladderOption:
                     System.out.println("ladder option");
                     if (currentPlayer == vikram) {
-                        vikramPosition+=dieNumber;
+                        vikramPosition += dieNumber;
                     }else {
-                        vedhaPosition+=dieNumber;
+                        vedhaPosition += dieNumber;
                     }
                     break;
+                    
                 case snakeOption:
                     System.out.println("snake option");
                     if (currentPlayer == vikram) {
-                        vikramPosition-=dieNumber;
+                        vikramPosition -= dieNumber;
                     }else {
-                        vedhaPosition-=dieNumber;
+                        vedhaPosition -= dieNumber;
                     }
-                    currentPlayer=switchPlayer(currentPlayer);
+                    currentPlayer = switchPlayer(currentPlayer);
                     break;
+                    
                 default:
                     System.out.println("No Play option");
-                    currentPlayer=switchPlayer(currentPlayer);
+                    currentPlayer = switchPlayer(currentPlayer);
                     break;
             }
 
             //ensure start position and exact winning position
+            
             if (vikramPosition < 0){
-                vikramPosition=0;
+                vikramPosition = 0;
             } else if (vedhaPosition < 0){
-                vedhaPosition=0;
-            }else if (vikramPosition>100) {
-                vikramPosition-=dieNumber;
-            } else if (vedhaPosition>100) {
-                vedhaPosition-=dieNumber;
+                vedhaPosition = 0;
+            }else if (vikramPosition > 100) {
+                vikramPosition -= dieNumber;
+            } else if (vedhaPosition > 100) {
+                vedhaPosition -= dieNumber;
             }
 
             System.out.println("current Vikram's position : "+vikramPosition);
             System.out.println("current Vedha's position : "+vedhaPosition);
 
-
         }
+        
         System.out.println("*******************************");
         showWinner(vikramPosition,vedhaPosition);
         System.out.println("Number of times die played = "+dieCount);
@@ -85,9 +88,9 @@ public class SnakeAndLadder {
     }
 
     static void showWinner(int vikram, int vedha){
-        if (vikram==100){
+        if (vikram == 100){
             System.out.println("Vikram is Winner");
-        } else if (vedha==100) {
+        } else if (vedha == 100) {
             System.out.println("Vedha is Winner");
         }
     }
