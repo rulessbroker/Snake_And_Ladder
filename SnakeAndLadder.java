fc03646a841da599168820db5816e390fd6cf9ea
rahulbhosale2563@gmail.com <samikshabhosale16@email.com>
2022-09-05 16:50:18 +0530

    Player rolls the die

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -9,5 +9,13 @@ public class SnakeAndLadder {
 		int position = startPosition;
 		
 		System.out.println("Current Position = " +position);
+		
+		int dieNumber=rollDie();
+
+	        System.out.println("Die Number = "+dieNumber);
+	    }
+	    static int rollDie(){
+	        int random= (int) (Math.random()*6) + 1;
+	        return random;
 	}
 }
